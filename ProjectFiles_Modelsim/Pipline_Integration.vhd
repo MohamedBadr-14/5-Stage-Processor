587699df0698082a3de009457e1aadbb34389406
Qabbani <omarmohamed20027@gmail.com>
2024-05-13 19:35:28 +0300

    fix1

--- ProjectFiles_Modelsim/Pipline_Integration.vhd
+++ ProjectFiles_Modelsim/Pipline_Integration.vhd
@@ -288,6 +288,7 @@ Architecture Pipeline_Integration_arch of Pipeline_Integration is
 	end component;
 
 	component Data_Memory is
+
 		port(
 			Rst,Clk         : in std_logic;
 			Mem_Write   	: in std_logic;
@@ -296,6 +297,7 @@ Architecture Pipeline_Integration_arch of Pipeline_Integration is
 			Mem_Read        : in std_logic;
 			Mem_Out         : out std_logic_vector(31 downto 0)
 		);
+
 	end component;
 
 	
@@ -444,7 +446,4 @@ begin
 
 	MemWrite_Final <= not(Prot_Reg_isProtected) AND MemWrite;
 
-
-	Data_Mem	: Data_Memory port map(reset,clk,MemWrite_Final,ALU_Res1,EX_MEM_Res2_Out,MemWrite_Final,dummy_MeM_Out);
-	
 end Pipeline_Integration_arch;
