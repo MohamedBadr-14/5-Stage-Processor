43dc82e68a30194940111340a94d4e4a8459b30a
Qabbani <omarmohamed20027@gmail.com>
2024-05-13 20:31:34 +0300

    Controller M port size

--- ProjectFiles_Modelsim/Controller.vhd
+++ ProjectFiles_Modelsim/Controller.vhd
@@ -10,7 +10,7 @@ entity Controller is
 		CCR_Write	: OUT std_logic_vector(3 DOWNTO 0); -- bit3 : OVF / bit2: CF / bit1 : NF / bit0 : ZF
 		EX 			: OUT std_logic_vector(3 DOWNTO 0); -- bit3 : ALUOp / bit2 : RegDst / bit1 : ALUSrc1 / bit0 : ALUSrc2
 		WB 			: OUT std_logic_vector(2 DOWNTO 0); -- bit2 : RegWrite1 / bit1 : RegWrite2/ bit0 : MemToReg
-		M 			: OUT std_logic_vector(1 DOWNTO 0); -- bit1 : Protect_Free / bit0 : PS_W_EN   
+		M 			: OUT std_logic_vector(3 DOWNTO 0); -- bit3 : MemWrite / bit2 : MemRead / bit1 : Protect_Free / bit0 : PS_W_EN      
 		IsInstOut	: OUT std_logic
     	);
 
