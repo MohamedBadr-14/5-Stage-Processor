1128489e022c351a4ec0b25821b8339631fba642
AleyElhiny <109850332+AleyElhiny@users.noreply.github.com>
2024-05-18 00:43:44 +0300

    Pop eshtaghalt elhamdullah

--- ProjectFiles_Modelsim/Data_Memory.vhd
+++ ProjectFiles_Modelsim/Data_Memory.vhd
@@ -12,8 +12,8 @@ entity Data_Memory is
 		Mem_Read        : in std_logic;
         Push_Pop        : in std_logic;
         SP_Enable       : in std_logic;
-		Mem_Out         : out std_logic_vector(31 downto 0);
-        Mem_outRange    : out std_logic
+		Mem_Out       : out std_logic_vector(31 downto 0) := x"00000000";
+        Mem_outRange    : out std_logic := '0'
 	);
 
 end entity;
@@ -23,18 +23,18 @@ Architecture Data_Memory_Arch of Data_Memory is
 	type data_mem is array(2048 to 4095) of std_logic_vector(15 downto 0);
 	signal data_array	: data_mem;
 
+    signal kofta    : std_logic_vector(31 downto 0);
+
 begin
 
-	process(ALL)
+	process(Rst,Clk)
 	begin
 
 		if Rst = '1' then
-			data_array <= (others => (others => '0'));
-            Mem_Out <= (others => '0');
+            --Mem_Out <= (others => '0'); XXXX are produced when uncommented, moshkela law el reset etrafa3 fel nos
             Mem_outRange <= '0';
-		end if;
-
-        if falling_edge(Clk) then
+			data_array <= (others => (others => '0'));
+		elsif falling_edge(Clk) then
             if Mem_Write = '1' then
                 if to_integer(unsigned(Address)) < 2048 or to_integer(unsigned(Address)) > 4095 then
                     Mem_outRange <= '1';
@@ -61,16 +61,33 @@ begin
             else
                 Mem_outRange <= '0';
                 if SP_Enable = '0' then
-                    Mem_Out(15 downto 0) <= data_array(to_integer(unsigned(Address)));
-                    Mem_Out(31 downto 16) <= data_array(to_integer(unsigned(Address)) + 1);
+                    Mem_Out <= data_array(to_integer(unsigned(Address)) + 1) & data_array(to_integer(unsigned(Address)));
                 else
                     if Push_Pop = '1' then -- Pop
-                        Mem_Out(31 downto 16) <= data_array(to_integer(unsigned(Address)));
-                        Mem_Out(15 downto 0) <= data_array(to_integer(unsigned(Address)) + 1);
+                    Mem_Out <= data_array(to_integer(unsigned(Address))) & data_array(to_integer(unsigned(Address)) + 1);
                     end if;
                 end if;
             end if;
         end if;
     end process;
+
+    -- Mem_outRange <= 
+    --                 '1' when to_integer(unsigned(Address)) < 2048 or to_integer(unsigned(Address)) > 4095
+    --                 else '0';
+
+
+    -- -- OutputMeM(15 downto 0) <= 
+    -- --                         data_array(to_integer(unsigned(Address))) when (Mem_Read = '1' and SP_Enable = '0')
+    -- --                         else data_array(to_integer(unsigned(Address)) + 1) when (Mem_Read = '1' and SP_Enable = '1' and Push_Pop = '1')
+    -- --                         else (others => '0');
+
+    -- -- OutputMeM(31 downto 16) <= 
+    -- --                         data_array(to_integer(unsigned(Address)) + 1) when (Mem_Read = '1' and SP_Enable = '0')
+    -- --                         else data_array(to_integer(unsigned(Address))) when (Mem_Read = '1' and SP_Enable = '1' and Push_Pop = '1')
+    -- --                         else (others => '0');
+    -- kofta <= data_array(2049) & data_array(2048);
+    -- OutputMeM <= kofta;
+    -- --OutputMeM(15 downto 0) <= data_array(2049);
+    -- --OutputMeM(31 downto 16) <= data_array(2048);
 
 end Data_Memory_Arch;
--- ProjectFiles_Modelsim/todo_memmm.do
+++ ProjectFiles_Modelsim/todo_memmm.do
@@ -1,5 +1,20 @@
+vsim -gui work.data_memory
+add wave -position insertpoint  \
+sim:/data_memory/Rst \
+sim:/data_memory/Clk \
+sim:/data_memory/Mem_Write \
+sim:/data_memory/Address \
+sim:/data_memory/Data \
+sim:/data_memory/Mem_Read \
+sim:/data_memory/Push_Pop \
+sim:/data_memory/SP_Enable \
+sim:/data_memory/OutputMeM \
+sim:/data_memory/Mem_outRange \
+sim:/data_memory/data_array
 force -freeze sim:/data_memory/Clk 0 0, 1 {500 ps} -r {1 ns}
 force -freeze sim:/data_memory/Rst 1 0
+run
+force -freeze sim:/data_memory/Rst 0 0
 force -freeze sim:/data_memory/Mem_Write 1 0
 force -freeze sim:/data_memory/Address 00000800 0
 force -freeze sim:/data_memory/Data 01234567 0
@@ -7,6 +22,6 @@ force -freeze sim:/data_memory/Mem_Read 0 0
 force -freeze sim:/data_memory/Push_Pop 0 0
 force -freeze sim:/data_memory/SP_Enable 0 0
 run
-force -freeze sim:/data_memory/Rst 0 0
 force -freeze sim:/data_memory/Mem_Write 0 0
 force -freeze sim:/data_memory/Mem_Read 1 0
+run
